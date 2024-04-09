33938a13ec159c904883ade9c2be56594affd5a3
marting07 <jose.martin07@gmail.com>
2024-04-09 17:21:03 -0600

    added chatui

---
+++ src/Dashboard/Chat/Messages.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Message from "./Message";
+
+const Messages = () => {
+    return <div className='chat_messages_container'>
+        <Message content='Hello ai' aiMessage={false} />
+        <Message animate content='Hello here is ai.' aiMessage={true} />
+    </div>;
+};
+
+export default Messages;
